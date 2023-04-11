19ea37bf910b0f215b48b606d28c33dc4ecba373
rdelena <rodnydelena@gmail.com>
2023-04-11 18:59:02 -0500

    Update index.js file to proper route. Create index.js for all routes. Use titles route & controller for universal route testing.

---
+++ controllers/titles.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+const pool = require("../sql/connection");
+const { handleSQLError } = require("../sql/error");
+
+//Gets all employees(Emp#, title, from_date, to_date) limit 1000.
+const getAllTitles = (req, res) => {
+  pool.query("SELECT * FROM titles LIMIT 1000;", (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+module.exports = {
+  getAllTitles,
+};
--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const express = require("express");
 const app = express();
 // Uses routes/employees
-const employeesRouter = require("./routes/employees");
-
 const port = process.env.PORT || 4001;
-
+const routes = require("./routes");
 
 // body parser substitute
 app.use(express.json());
 // uses employees router
-app.use("/employees", employeesRouter);
+app.use(routes);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to our server!");
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-=======
 app.get("/", (req, res) => res.send("Welcome to our API!"));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
