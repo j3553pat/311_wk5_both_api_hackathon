4d74650eb307ae270bd104b0f01490001b778fe4
egarza27 <elizabethigarza10@gmail.com>
2023-04-10 20:20:26 -0500

    added basic express and initialized app

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('re-testing')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.get("/", (req, res) => res.send("Welcome to our API!"));
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
