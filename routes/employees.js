c551d4620f3ebbd90b777bc121dda8a92869e956
DKYoungOne <bawsinator@gmail.com>
2023-04-10 20:46:04 -0500

    Merge main branch for connections

--- routes/employees.js
+++ routes/employees.js
@@ -1,7 +1,9 @@
-const express = require('express')
-router.getEmployeesById('/employees/:id', controller.getEmployeesById)
+
+
 const express = require('express')
 const employeesController = require('../controllers/employees')
 const router = express.Router()
-
+router.getEmployeesById('/employees/:id', controller.getEmployeesById)
 module.exports = router
+
+
