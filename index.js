af2f51de553bd16a5155c29190664302945034bc
egarza27 <elizabethigarza10@gmail.com>
2023-04-11 18:18:27 -0500

    added departments controller method and route

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const departmentsRouter = require("./routes/departments");
 const app = express();
+
 const port = process.env.PORT || 4001;
 
 app.get("/", (req, res) => res.send("Welcome to our API!"));
