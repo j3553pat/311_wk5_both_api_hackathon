7fed531249d91ca6c58b8a419a68806d3ae98cbf
mannybennett <mannybennett99@gmail.com>
2023-04-11 21:50:27 -0400

    wip

---
+++ controllers/salaries.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+const pool = require("../sql/connection");
+const { handleSQLError } = require("../sql/error");
+
+const getSalaryByEmployeeName = (req, res) => {
+  console.log(req)
+  let sql = "SELECT salary FROM salaries JOIN employees ON salaries.emp_no = employees.emp_no WHERE first_name = ? LIMIT 1000;"
+  sql = mysql.format(sql, [req.params.first_name])
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json(results);
+  })
+}
+
+module.exports = {
+  getSalaryByEmployeeName,
+};
