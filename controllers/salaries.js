3521946adbc732766d6b3c1a6b80aa07bf279f4f
mannybennett <mannybennett99@gmail.com>
2023-04-11 22:57:16 -0400

    Create getSalaryByEmployeeName controller/route + salaries files

--- controllers/salaries.js
+++ controllers/salaries.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 const pool = require("../sql/connection");
 const { handleSQLError } = require("../sql/error");
 
+//Get list of employee's salaries by first_name
 const getSalaryByEmployeeName = (req, res) => {
   let sql = "SELECT salary FROM salaries JOIN employees ON salaries.emp_no = employees.emp_no WHERE first_name = ? LIMIT 1000;"
   sql = mysql.format(sql, [req.params.first_name])
--- routes/salaries.js
+++ routes/salaries.js
@@ -2,6 +2,7 @@ const express = require("express");
 const salariesController = require("../controllers/salaries");
 const router = express.Router();
 
+// Using: localhost:4001/salaries/firstname/*EMPLOYEE FIRST NAME HERE*
 router.get("/firstname/:first_name", salariesController.getSalaryByEmployeeName);
 
 module.exports = router;
